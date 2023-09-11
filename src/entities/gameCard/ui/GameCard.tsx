950b6e913fcaf4f739f97324df1283c3c3cdc79e
Andrey Evstafyev <ean@d-element.ru>
2023-09-11 10:44:59 +0500

    fix

--- src/entities/gameCard/ui/GameCard.tsx
+++ src/entities/gameCard/ui/GameCard.tsx
@@ -1,8 +1,15 @@
-import { Game } from "@shared/types";
-import { Card } from "antd";
+import {Game} from "@shared/types";
+import {Card} from "antd";
+import React from "react";
 
 type GameCard = Omit<Game, "short_description" | "genre" | "platform" | "developer">;
 
-export const GameCard: React.FC = ({ id, title, release_date: releaseDate, thumbnail, publisher }: GameCard) => {
-  return <>{title}</>;
+export const GameCard: React.FC<GameCard> = ({
+                                               id,
+                                               title,
+                                               release_date: releaseDate,
+                                               thumbnail,
+                                               publisher
+                                             }) => {
+  return <Card title={title}/>
 };
